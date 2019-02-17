a63363ac25ddf2014773643c696af85d9ed53425
Ciaran Callaghan <ciarancallaghan94@gmail.com>
2019-02-17 17:41:15 +0000

    Staircase

---
+++ Python3/Algorithms/1-Easy/Staircase/answer.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the staircase function below.
+def staircase(n):
+    spaces = n-1
+    hashes = 1
+    while spaces >= 0:
+        print((' ' * spaces) + ('#' * hashes))
+        hashes += 1
+        spaces -= 1
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
