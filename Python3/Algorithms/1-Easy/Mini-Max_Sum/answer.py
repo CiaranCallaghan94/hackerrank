bb808e05200b9b27e1f7dc2feba86f36018a3c64
Ciaran Callaghan <ciarancallaghan94@gmail.com>
2019-02-17 17:50:15 +0000

    Mini-Max Sum

---
+++ Python3/Algorithms/1-Easy/Mini-Max_Sum/answer.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr):
+    size = len(arr)
+    arr.sort()
+    min = sum(arr[0:size-1])
+    max = sum(arr[1:size])
+
+    print(f"{min} {max}")
+
+
+
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
